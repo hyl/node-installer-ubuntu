6ef6519d0e6afc49dc6286805da0293b75457a0d
Jamie Hoyle <j@jamiehoyle.com>
2014-07-13 12:36:40 +0100

    Removed duplicate apt-get

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #RUN AS ROOT/SUDO 
 sudo apt-get update #force latest package list to be intalled
-sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
